4663f5dad8f738e228709b4f0e2b85b355ab60f5
eiiizong <eiiizong@163.com>
2022-11-28 20:51:41 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
