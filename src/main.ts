8b6ca35973e7b611601b720d5e9c2af593109c06
eiiizong <eiiizong@163.com>
2022-11-29 16:18:44 +0800

    框架搭建

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 
 import App from '@/App.vue'
+// 引入路由
 import router from '@/router'
+// 引入全局状态管理
 import pinia from '@/stores'
+
+// 引入样式
+import 'normalize.css/normalize.css'
 
 const app = createApp(App)
 
