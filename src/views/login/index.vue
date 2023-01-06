4728c92d628c5a3be416625e69776e83cbf64203
eiiizong <eiiizong@163.com>
2023-01-06 20:22:54 +0800

    框架搭建

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">登录</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.login {
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  padding: 20px;
+}
+</style>
---
+++ src/views/register/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="register">登录</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.register {
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  padding: 20px;
+}
+</style>
