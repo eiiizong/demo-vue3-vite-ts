ec5b3717935ef1d50106eab1549cbad33fc844d2
eiiizong <eiiizong@163.com>
2023-01-06 21:40:37 +0800

    框架搭建

---
+++ src/views/login/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-form" :style="loginFormStyle">登录</div>
+</template>
+
+<script setup lang="ts">
+import imageBgRight from '../images/bg-right.png'
+
+import { ref, computed } from 'vue'
+
+const loginFormStyle = computed(() => {
+  return `background-image: url(${imageBgRight});`
+})
+</script>
+
+<style lang="scss" scoped>
+.login-form {
+  width: 640px;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-start;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  transition: all 0.3s;
+}
+</style>
